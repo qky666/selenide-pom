40b7f49d1ee2adc63cc2dab7a8647c75f0437e55
jose.torrecilla@gmail.com <jose.torrecilla@gmail.com>
2022-01-02 16:59:54 +0100

    Fixed gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.qky666"
-version = "0.8.0"
+version = "0.8.1"
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.github.qky666"
             artifactId = "selenide-pom"
-            version = "0.8.0"
+            version = "0.8.1"
 
             from(components["java"])
         }
@@ -26,12 +26,11 @@ plugins {
     `maven-publish`
     kotlin("jvm") version kotlinVersion
     id("io.freefair.lombok") version "6.3.0"
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.40.0"
 }
 
 dependencies {
     val kotlinVersion = "1.6.10"
-    val jUnitVersion = "5.8.2"
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
@@ -44,14 +43,8 @@ dependencies {
     // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging
     implementation("io.github.microutils:kotlin-logging:2.1.21")
 
-    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
-
-    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-engine
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
-
-    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-params
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
+    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
 }
