5590a62fa236ac5493664883ca18f9b5a9110b46
jose.torrecilla@mtp.es <jose.torrecilla@mtp.es>
2022-09-12 15:36:48 +0200

    v0.12.0 Changed version and updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.qky666"
-version = "0.11.0"
+version = "0.12.0"
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.github.qky666"
             artifactId = "selenide-pom"
-            version = "0.11.0"
+            version = "0.12.0"
 
             from(components["java"])
         }
@@ -25,18 +25,18 @@ plugins {
     `java-library`
     `maven-publish`
     kotlin("jvm") version kotlinVersion
-    id("io.freefair.lombok") version "6.5.0.3"
+    id("io.freefair.lombok") version "6.5.1"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 dependencies {
     val kotlinVersion = "1.7.10"
-    val jUnitVersion = "5.8.2"
+    val jUnitVersion = "5.9.0"
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    implementation("com.codeborne:selenide:6.6.6")
+    implementation("com.codeborne:selenide:6.7.4")
     implementation("io.github.microutils:kotlin-logging:2.1.23")
-    testImplementation("org.slf4j:slf4j-simple:1.7.36")
+    testImplementation("org.slf4j:slf4j-simple:2.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
