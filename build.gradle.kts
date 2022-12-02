f3da62d3f36468494ea4b538d2f3ffe02c761874
jose.torrecilla@mtp.es <jose.torrecilla@mtp.es>
2022-12-02 12:46:58 +0100

    Changes in TestData

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.qky666"
-version = "0.14.1"
+version = "0.15.0"
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.github.qky666"
             artifactId = "selenide-pom"
-            version = "0.14.1"
+            version = "0.15.0"
 
             from(components["java"])
         }
