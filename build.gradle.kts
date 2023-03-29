21a692c52c1c6c86eb02aa58a8e0a61233f79d12
qky669 <qky669@gmail.com>
2023-03-29 14:00:19 +0200

    build.gradle.kts simplified

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,10 @@ val myVersion = "0.16.0"
 group = myGroup
 version = myVersion
 
-val kotlinVersion = "1.8.10"
-
 plugins {
-    val kotlinVersion = "1.8.10"
-
     `java-library`
     `maven-publish`
-    kotlin("jvm") version kotlinVersion
+    kotlin("jvm") version "1.8.10"
     id("io.freefair.lombok") version "8.0.1"
     id("com.github.ben-manes.versions") version "0.46.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
@@ -38,14 +34,14 @@ repositories {
 dependencies {
     val jUnitVersion = "5.9.2"
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation(kotlin("reflect"))
     implementation("com.codeborne:selenide:6.12.4")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
     testImplementation("org.slf4j:slf4j-simple:2.0.7")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+    testImplementation(kotlin("test"))
 }
 
 kotlin {
