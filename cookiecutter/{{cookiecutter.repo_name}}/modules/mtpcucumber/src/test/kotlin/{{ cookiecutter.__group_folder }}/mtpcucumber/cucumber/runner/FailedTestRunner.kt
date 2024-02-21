9c58a6e4eb3db854c28ce5b6b48c07cbeda828c2
jose.torrecilla <jose.torrecilla@mtp.es>
2024-02-21 12:44:34 +0100

    Multiproject template

--- cookiecutter/{{cookiecutter.repo_name}}/modules/mtpcucumber/src/test/kotlin/{{ cookiecutter.__group_folder }}/mtpcucumber/cucumber/runner/FailedTestRunner.kt
+++ cookiecutter/{{cookiecutter.repo_name}}/modules/mtpcucumber/src/test/kotlin/{{ cookiecutter.__group_folder }}/mtpcucumber/cucumber/runner/FailedTestRunner.kt
@@ -7,10 +7,8 @@ import org.testng.annotations.Test
 @Test
 @CucumberOptions(
     features = ["@build/test-results/cucumber/failed_scenarios.txt"],
-    // glue = ["{{ cookiecutter.group }}.mtpcucumber.cucumber.steps"],
-    glue = ["my.group.template.mtpcucumber.cucumber.steps"],
-    // plugin = ["{{ cookiecutter.group }}.common.cucumber.listener.CucumberListener", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty", "json:build/test-results/cucumber/rerun.json", "rerun:build/test-results/cucumber/failed_scenarios.txt"],
-    plugin = ["my.group.template.common.cucumber.listener.CucumberListener", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty", "json:build/test-results/cucumber/rerun.json", "rerun:build/test-results/cucumber/failed_scenarios.txt"],
+    glue = ["{{ cookiecutter.group }}.mtpcucumber.cucumber.steps"],
+    plugin = ["{{ cookiecutter.group }}.common.cucumber.listener.CucumberListener", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty", "json:build/test-results/cucumber/rerun.json", "rerun:build/test-results/cucumber/failed_scenarios.txt"],
     tags = "@retry",
 )
 class FailedTestRunner : BaseTestRunner()
--- cookiecutter/{{cookiecutter.repo_name}}/modules/mtpcucumber/src/test/kotlin/{{ cookiecutter.__group_folder }}/mtpcucumber/cucumber/runner/WipTestRunner.kt
+++ cookiecutter/{{cookiecutter.repo_name}}/modules/mtpcucumber/src/test/kotlin/{{ cookiecutter.__group_folder }}/mtpcucumber/cucumber/runner/WipTestRunner.kt
@@ -8,10 +8,8 @@ import org.testng.annotations.Test
 @Test
 @CucumberOptions(
     features = ["src/test/resources/features"],
-    // glue = ["{{ cookiecutter.group }}.mtpcucumber.cucumber.steps"],
-    glue = ["my.group.template.mtpcucumber.cucumber.steps"],
-    // plugin = ["{{ cookiecutter.group }}.common.cucumber.listener.CucumberListener", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty", "json:build/test-results/cucumber/run.json", "rerun:build/test-results/cucumber/failed_scenarios.txt"],
-    plugin = ["my.group.template.common.cucumber.listener.CucumberListener", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty", "json:build/test-results/cucumber/run.json", "rerun:build/test-results/cucumber/failed_scenarios.txt"],
+    glue = ["{{ cookiecutter.group }}.mtpcucumber.cucumber.steps"],
+    plugin = ["{{ cookiecutter.group }}.common.cucumber.listener.CucumberListener", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty", "json:build/test-results/cucumber/run.json", "rerun:build/test-results/cucumber/failed_scenarios.txt"],
     tags = "@wip",
 )
 open class WipTestRunner : BaseTestRunner()
