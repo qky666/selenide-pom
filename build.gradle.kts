be424b7480deaddb02aa48d1e5ce73c9a1164f3a
qky669 <qky669@gmail.com>
2025-06-10 07:20:47 +0200

    0.30.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 group = "com.github.qky666"
-version = "0.30.3"
+version = "0.30.4"
 
 val javaVersionNumber = 21
 
@@ -8,7 +8,7 @@ plugins {
     `java-library`
     // jitpack needs maven-publish plugin
     `maven-publish`
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
     id("io.freefair.lombok") version "8.13.1"
     id("com.github.ben-manes.versions") version "0.52.0"
 }
@@ -29,14 +29,14 @@ repositories {
 }
 
 dependencies {
-    val jUnitVersion = "5.12.1"
+    val jUnitVersion = "5.13.1"
 
     implementation(kotlin("reflect"))
     implementation(kotlin("test"))
-    implementation("com.codeborne:selenide-appium:7.8.1")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.6")
+    implementation("com.codeborne:selenide-appium:7.9.3")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
     implementation("org.bytedeco:javacv-platform:1.5.11")
-    implementation("net.sourceforge.tess4j:tess4j:5.15.0")
+    implementation("net.sourceforge.tess4j:tess4j:5.16.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.slf4j:slf4j-simple:2.0.17")
