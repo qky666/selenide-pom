f241e6d6ea5eb7f8c632a7b352ff63bb889b1280
qky669 <qky669@gmail.com>
2025-08-14 13:45:15 +0200

    0.30.5. Updated gradle to 9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
     testImplementation("com.google.code.findbugs:jsr305:3.0.2")
-    testImplementation("org.bytedeco:javacv-platform:1.5.12")
 }
 
 kotlin {
@@ -80,22 +79,4 @@ tasks.compileJava {
 
 tasks.compileTestJava {
     options.compilerArgs.addAll(listOf("-encoding", "UTF-8"))
-}
-
-tasks {
-    withType<Jar> {
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE // allow duplicates
-        // Otherwise you'll get a "No main manifest attribute" error
-        manifest {
-            attributes["Main-Class"] = "org.bytedeco.javacv.samples.Demo"
-        }
-
-        // To add all the dependencies otherwise a "NoClassDefFoundError" error
-        from(sourceSets.main.get().output)
-
-        dependsOn(configurations.runtimeClasspath)
-        from({
-                 configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-             })
-    }
 }
